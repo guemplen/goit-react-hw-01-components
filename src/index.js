c50399f0b828e4655f05663c00fa99515b85d856
guemplen <guemplen@gmail.com>
2023-09-25 22:24:47 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import Profile from './Profile/Profile';
+import user from './Profile/user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   </React.StrictMode>
 );
+
+export default Profile;
