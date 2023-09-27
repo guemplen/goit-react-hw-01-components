b271756f6f1147452fbab498f77fc20f3cf57d13
guemplen <guemplen@gmail.com>
2023-09-27 13:46:58 +0200

    Revert "task1"
    This reverts commit c50399f0b828e4655f05663c00fa99515b85d856.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import Profile from './Profile/Profile';
-import user from './Profile/user.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <App />
   </React.StrictMode>
 );
-
-export default Profile;
